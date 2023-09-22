ec5393d2eb7ca02f8fc75b5c8d62028768d187ea
Aperture-IT-Solutions <brunosantos@v3hfv.onmicrosoft.com>
2023-09-21 22:16:18 -0300

    alterando localização

--- s3/s3.tf
+++ s3/s3.tf
@@ -1,62 +1,49 @@
-resource "aws_s3_bucket" "my-static-website" {
-  bucket = "my-static-jenkinss34494498" # give a unique bucket name
-  tags = {
-    Name = "my-static-jenkinss34494498"
-  }
-}
+resource "aws_s3_bucket" "website" {
+  bucket = "myjenkinsbucketlast"
+  acl = "public-read"
 
-resource "aws_s3_bucket_website_configuration" "my-static-website" {
-  bucket = aws_s3_bucket.my-static-website.id
-
-  index_document {
-    suffix = "index.html"
+  tags {
+    Name = "Website"
+    Environment = "production"
   }
 
-  error_document {
-    key = "index.html"
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT","POST"]
+    allowed_origins = ["*"]
+    expose_headers = ["ETag"]
+    max_age_seconds = 3000
   }
-}
 
-resource "aws_s3_bucket_versioning" "my-static-website" {
-  bucket = aws_s3_bucket.my-static-website.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# S3 bucket ACL access
-
-resource "aws_s3_bucket_ownership_controls" "my-static-website" {
-  bucket = aws_s3_bucket.my-static-website.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "my-static-website" {
-  bucket = aws_s3_bucket.my-static-website.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_acl" "my-static-website" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.my-static-website,
-    aws_s3_bucket_public_access_block.my-static-website,
+  policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadForGetBucketObjects",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::myjenkinsbucketlast/*"
+    }
   ]
+}
+EOF
 
-  bucket = aws_s3_bucket.my-static-website.id
-  acl    = "public-read"
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
 }
 
+# AWS S3 bucket for www-redirect
+resource "aws_s3_bucket" "website_redirect" {
+  bucket = "www.${var.website_bucket_name}"
+  acl = "public-read"
 
-
-
-# s3 static website url
-
-output "website_url" {
-  value = "http://${aws_s3_bucket.my-static-website.bucket}.s3-website.${var.region}.amazonaws.com"
+  website {
+    redirect_all_requests_to = "${var.website_bucket_name}"
+  }
 }
